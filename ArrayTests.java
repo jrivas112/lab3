ebed5f8b99a2e6881d0eabb0dd03060b8ef69423
Julio Rivas <rivasjulioedu@gmail.com>
2023-11-05 13:13:29 -0800

    n/a

--- ArrayTests.java
+++ ArrayTests.java
@@ -2,17 +2,5 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class ArrayTests {
-	@Test 
-	public void testReverseInPlace() {
-    int[] input1 = { 3 };
-    ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 3 }, input1);
-	}
 
-
-  @Test
-  public void testReversed() {
-    int[] input1 = { };
-    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
-  }
 }
